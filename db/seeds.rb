4de56540029024469bc34c60abbd59dfb769c855
Lloyd Chapple <lloydchaps@gmail.com>
2020-12-17 13:39:03 +0000

    backend up and running, time for some design

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,7 @@ require 'open-uri'
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients_serialized = open(url).read
 ingredients_array = JSON.parse(ingredients_serialized)['drinks'].map { |ingredient| ingredient.values.join }
-p ingredients_array
 
+ingredients_array.each do |ingredient|
+  Ingredient.create(name: "#{ingredient}")
+end
